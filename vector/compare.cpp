1982f2192015094953105f087d2612d612699b32
beva3 <mizabienvenu@gmail.com>
2024-02-12 16:35:15 +0300

    chercher min

--- vector/compare.cpp
+++ vector/compare.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+
+class V{
+    private:
+        string minV(vector <string> list){
+            string min = "";
+
+            for(auto str : list){
+                if(min > str)
+                    min = str;
+            }
+
+            return min;
+        }
+    public:
+        V(){}
+        ~V(){};
+        void misonga(){
+            vector <string> finale;
+        }
+        vector <string> liste;
+
+};
+string minV(vector <string> list){
+    string min = list[0];
+
+    for(int i = 0 ; i < list.size() ; i++){
+        if(list[i] < min)
+            min = list[i];
+    }
+
+    return min;
+}
 
 
 int main(){
 
     string s1 = "aaphael";
     string s2 = "raphael";
+    vector <string> names = {
+        "abel",
+        "beli",
+        "ced",
+        "aaa"
+    };
 
-    cout << s1 + s2 << endl;
+    cout <<minV(names) << endl;
+
+    /* cout << s1 + s2 << endl;
     if(s1 > s2)
         cout << s1 << " est sup "<< endl;
     else 
         cout << s2 << " est sup "<< endl;
-
+ */
     return 0;
 }
