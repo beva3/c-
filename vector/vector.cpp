14b9a3732d790b0ab9c1232d75270d67557d70e8
beva3 <mizabienvenu@gmail.com>
2024-02-12 16:03:20 +0300

    apprendre vector

---
+++ vector/vector.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	vector <string> semain  = {"monday","sunday","thusday"};
+
+	cout << semain.size() << endl;
+	
+	return 0;
+}
