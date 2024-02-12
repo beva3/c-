f4ec66fcdbd9adddf22e9b9bbb6eb15d6c948940
beva3 <mizabienvenu@gmail.com>
2024-02-12 10:41:34 +0300

    trier les caracters par decroisantes

--- trii/triiCaracter.cpp
+++ trii/triiCaracter.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int main(){
 
-    string word1 = "dbca";
+    string word1  = "dbca";
+    string word2  = "dbca";
     sort(word1.begin(), word1.end());
+    sort(word2.begin(), word2.end(),greater<int>());
 
     cout << word1 << endl;
+    cout << word2 << endl;
 
     return 0;
 }
